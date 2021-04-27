c5aede8e87457d11908377abd10dbd7338d531fa
izunna aliozor <me@gmail.com>
2021-04-26 13:59:38 -1200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+
+define('DOT', '.');
+require_once DOT . "/bootstrap.php";
+
+//Home page//
+$Route->add('/anthilltutors/', function () {
+    
+    $Template = new Apps\Template;
+    $Template->addheader("docs.layouts.header");
+    $Template->addfooter("docs.layouts.footer");
+    $Template->assign("title","Anthill Home");
+
+    $Template->render("docs.index");
+
+}, 'GET');
+//Home page//
+
+
+
+
+
+//Logout Sessions//
+$Route->add(
+    '/auth/logout',
+    function () {
+        $Template = new Apps\Template;
+        $Template->expire();
+        $Template->cleanAll(session_delete_timout);
+        $Template->redirect(auth_url);
+    },
+    'GET'
+);
+//Logout Sessions//
+
+
+
+$Route->run('/');
