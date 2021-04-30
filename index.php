0a9a274828e54136e3750aea30da26d45074d0dc
izunna aliozor <me@gmail.com>
2021-04-29 19:55:50 -1200

    Created new work

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 use Apps\Core;
 
-use function PHPSTORM_META\elementType;
-
 define('DOT', '.');
 require_once DOT . "/bootstrap.php";
 
@@ -13,12 +11,12 @@ $Route->add('/social_group/', function () {
     $Template = new Apps\Template;
     $Template->addheader("layouts.header");
     $Template->addfooter("layouts.footer");
-    $Template->assign("title","Home");
+    $Template->assign("title","SOCIALY");
 
     $Template->render("home");
 
 }, 'GET');
-<<<<<<< HEAD
+
 $Route->add('/social_group/register', function () {
     
     $Template = new Apps\Template;
@@ -32,62 +30,83 @@ $Route->add('/social_group/register', function () {
 
 
 $Route->add('/social_group/dashboard', function () {
-     $Core = new Apps\Core; 
     $Template = new Apps\Template;
+    $Core = new Apps\Core;
     $Template->addheader("layouts.header");
     $Template->addfooter("layouts.header");
     $Template->assign("title","dashboard");
-    $selects = $Core->Selectmember();
-
-    $Template->assign("select","$selects");
-
-    $Template->render("dasboard");
-
-
+    $Members = $Core->Getregistered();
+    $Template->assign("Members", $Members);
+    $Template->render("dashboard");
 },'GET');
 
 
 
-$Route->add('/social_group/forms/register',function(){
-    $Template = new Apps\Template;
-    $Core = new Apps\Core;
-    $Data = $Core->data;
-    $fname = $Data->fname;
-    $lname = $Data->lname;
-    $email = $Data->email;
-    $dateofbirth= $Data->dateofbirth;
-    $mobile = $Data->mobile;
-    $password = $Data->password;
-    $repeatpassword = $Data->repeatpassword;
-    $id = $Core->RegisterMembers($fname,$lname,$email,$mobile,$dateofbirth,$password,$repeatpassword);
-    if($id){
-        $Template->setError("Registered successfully" ,"success" ,"/social_group/dashboard");
-        $Template->redirect("/social_group/dashboard");
-    }else {
-        $Template->setError("Registration failed ! Try again.","warning","/social_group/forms/register");
-        $Template->redirect("/social_group/forms/register");
-    }
-},'POST');
-//Home page//
-=======
->>>>>>> 3d6c723cf82c032836149b346a8ed08405a31017
 
 
 $Route->add('/social_group/login', function () {
-    
     $Template = new Apps\Template;
     $Template->addheader("layouts.header");
     $Template->addfooter("layouts.footer");
     $Template->assign("title","login");
-
     $Template->render("login");
-
 }, 'GET');
-//Home page//
+
+
+
+$Route->add('/social_group/forms/login', function () {
+    $Core = new Apps\Core; 
+    $Template = new Apps\Template;
+    $Data = $Core->data;
+    $email = $Data->email;
+    $password = $Core->password1tohash($Data->password);
+    $login = $Core->UserLogin($email, $password);
+    if ($login->id) {
+        $Template->authorize($login->id);
+
+    $Template->setError("Login Successful","success","/social_group/dashboard");
+    $Template->redirect("/social_group/dashboard");
+
+    }else{
+        $Template->setError("Invalid Email or Password !","warning","/social_group/login");
+        $Template->redirect("/social_group/login");
+    }
+    
+    
+}, 'POST');
+
+
+
+$Route->add('/social_group/forms/register', function(){
+$Core = new Apps\Core;
+$Template = new Apps\Template;
+$Data = $Core->data;
+$email = $Data->email;
+$fname = ucfirst($Data->fname);
+$lname = ucfirst($Data->lname);
+$dateofbirth = $Data->dateofbirth;
+$mobile = $Data->mobile;
+$password = $Core->password1tohash($Data->password);
+$repeatpassword = $Core->password1tohash($Data->repeatpassword);
+ if($password === $repeatpassword){
+    $id = $Core->RegisterMembers($fname,$lname,$email,$mobile,$dateofbirth,$password);
+    if($id){
+        $Template->setError("Registered successfully", "success", "/social_group/login");
+        $Template->redirect("/social_group/login");
+    }
+        $Template->setError("Registration failed!, some credentials already in use retry.", "warning", "/social_group/register");
+        $Template->redirect("/social_group/register");
+    } 
+
+    $Template->setError("Password did not match","warning","/social_group/register");
+    $Template->redirect("/social_group/register");
+},'POST');
+
+
 
 //Logout Sessions//
 $Route->add(
-    '/auth/logout',
+    '/social_group/logout',
     function () {
         $Template = new Apps\Template;
         $Template->expire();
--- templates/register.php
+++ templates/register.php
@@ -1,35 +1,40 @@
-<form action="/social_group/forms/register" method="POST">
-  <div class="container-fluid" class="col-sm-0">
-    <h1 class="text-center" style="font-size:70px;"><u>Register</u></h1>
-    <p class="text-center">Please fill in this form to create an account.</p>
-    <hr >
-<div class="col-sm-4">
-    <label for="firstname"><b>Firstname:</b></label>
-    <input type="text"  name="Firstname" ></div>
-<div class="col-sm-4">
-    <label for="lastname"><b>Lastname:</b></label>
-    <input type="text"  name=" Lastname" ><br></div>
-<div class="col-sm-4">
-    <label for="email"><b>Email:  </b></label>
-    <input type="text"  name="email" ><br></div>
-<div class="col-sm-4">
-    <label for="Dateborn"><b>Date of birth:</b></label>
-    <input type="text"  name="date of birth" ><br></div>
-<div class="col-sm-4">
-    <label for="Dateborn"><b>Mobile:</b></label>
-    <input type="tel"  name="number" ></div><br>
-<div class="col-sm-4">
-    <label for="psw"><b>Password:</b></label>
-    <input type="password"  name="psw"><br></div>
-<div class="col-sm-4">
-    <label for="psw-repeat"><b>Repeat Password:</b></label>
-    <input type="password"  name="psw-repeat" ><br></div>
-    <hr>
-<div class="col-sm-4">
-    <button type="submit" class="registerbtn" value="register">Register</button></div>
-  </div>
 
-  <div class="container signin">
-    <p>Already have an account? <a href="./">Sign in</a>.</p>
-  </div>
-</form>
+
+            <form action="./forms/register" method="POST">
+            <h2><?= $self->Toast();?></h2>
+            <img src="<?= $assets ?>/avatar/images.png" class="icard-mg-top" style="height: 30px; width:90px;" alt="...">
+            <h1> <b>Open New Account Here</b> </h1>
+            
+                <div class="row">
+                    <div class="col-12 col-sm-6">
+                        <input type="text" class="form-control" name="fname" placeholder="First name"  autofocus required>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <input type="text" class="form-control" name="lname" placeholder="Last name"  required>
+                    </div>
+                </div><br>
+                <div class="row">
+                    <div class="col-12 col-sm-6">
+                        <input type="email" class="form-control"  name="email" placeholder="E-mail Address"   required>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <input type="text" class="form-control" name="dateofbirth" placeholder="Date of birth"  required>
+                    </div>
+                </div><br>
+                <div class="row">
+                <div class="col-12 col-sm-6">
+                        <input type="password" class="form-control" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <input type="tel" class="form-control" name="mobile" placeholder="Phone Number" required>
+                    </div>
+                    <br>
+                    <div class="col-12 col-sm-6">
+                        <input type="password" class="form-control" name="repeatpassword" placeholder="Confirm Password" required>
+                    </div>
+                </div><br>
+            <input type="submit" value="Sign Up">
+                
+                </div>
+            </form>
+            </div>
